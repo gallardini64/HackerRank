e1fa419da3c2ebe55a7964165a512b6bf3755351
gallardini <lgallardini@totalcoin.com>
2022-06-21 18:27:41 -0300

    added the other things

---
+++ EjerciciosLogicos/NoticiasWeb.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace EjerciciosLogicos
+{
+    //Una empresa de noticias ofrece un servicio suscripción el cual este compuesto por 3 niveles de membresía 
+    //Gratis
+    //Básico
+    //Premium
+    //Los tiempos de carga para ver las noticias varían según el plan de suscripción 30, 10 y 1 segundo respectivamente
+    //las empresas interesadas son:
+    //Facebook -- premium 
+    //Google -- basico
+    //Amazon -- gratis
+    //Completa el codigo necesario para que las empresas puedan recibir noticias 
+    //Para calcular el tiempo utiliza la funcion Thread.Sleep(tiempo int en milisegundos)  
+
+    public class NoticiasWeb
+    {
+        public List<Noticia> Noticias { get; set; }
+        public List<Noticia> EntregarNoticias()
+        {
+            return Noticias;
+        }
+    }
+    public abstract class Suscriptor
+    {
+        public List<Noticia> Noticias { get; set; }
+        public Func<List<Noticia>> DelegateRecibirNoticias;
+        public Plan Plan { get; private set; }
+        public virtual void RecibirNoticias(Plan plan)
+        {
+            Thread.Sleep(0);
+        }
+    }
+    
+
+    public class Noticia
+    {
+        public string Contenido { get; set; }
+    }
+
+    public class Facebook 
+    {
+        
+    }
+    public class Google
+    {
+
+    }
+    public class Amazon
+    {
+
+    }
+
+    public enum Plan
+    {
+        Premium,
+        Basico,
+        Gratis
+    }
+
+}
---
+++ TestLogica/TestNoticiasWeb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLogica
+{
+    [TestClass]
+    public class TestNoticiasWeb
+    {
+        [TestMethod]
+        public Task TestSuscripciones()
+        {
+
+            return Task.FromResult(0);
+        }
+    }
+}
