ff5a025a5298e0c79b68c2e28fc4b32f273aebe4
gallardini <lgallardini@totalcoin.com>
2022-07-01 01:39:15 -0300

    time convertion

---
+++ EjerciciosLogicos/TimeConvertion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EjerciciosLogicos
+{
+    //Dado un tiempo en el formato 12-horas AM/PM formato, convertir en formato militar (24 horas)
+    //Nota: 12:00:00AM on a 12 hour clock is 00:00:00 on a 24-hour clock.
+    //Ejemplo
+    //s = 12:01:00PM
+    //return 12:01:00
+    //s = 12:01:00AM
+    //return 00:01:00
+    //Crear un metodo estatico publico Convertir que reciba un string representando una fecha y retorne un string 
+    public class TimeConvertion
+    {
+        public static string Convertir(string fechaAMPM)
+        {
+            var fecha = DateTime.Parse(fechaAMPM);
+            return fecha.ToString("HH:mm:ss");
+        }
+
+    }
+}
---
+++ TestLogica/TestTimeConvertion.cs
@@ -0,0 +1,18 @@
+﻿using EjerciciosLogicos;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestLogica
+{
+    [TestClass]
+    public class TestTimeConvertion
+    {
+        [TestMethod]
+        [DataRow("12:01:00PM","12:01:00")]
+        [DataRow("12:01:00AM","00:01:00")]
+        public void TestConvertion(string date, string expected)
+        {
+            var result = TimeConvertion.Convertir(date);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
